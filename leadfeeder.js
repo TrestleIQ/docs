8dfdab97d75337ac5d8e90e29ca653c3c308e5bf
trestle-joeqiu <jqiu@trestleiq.com>
2026-01-05 11:01:38 -0800

    Fixing Dealfront
    - fixing Dealfront tracker script
    - update docs.json to use js

--- leadfeeder.js
+++ leadfeeder.js
@@ -3,17 +3,16 @@
 // and sends visitor data to Dealfront for lead generation insights.
 // Learn more: https://www.dealfront.com/leadfeeder/
 
-(function () {
-    // Create script element for Leadfeeder tracker
-    var script = document.createElement('script');
-    script.src = 'https://www.leadfeeder.com/tracker/crhzyCHlh3CL1Bpny5ewwINdZ1EWnALPyjKtXof9h8g.js';
-    script.async = true;
-
-    // Append to document head
-    document.head.appendChild(script);
-
-    // Initialize Leadfeeder queue for any custom tracking calls
-    window.lf = window.lf || function () {
-        (window.lf.q = window.lf.q || []).push(arguments);
+(function(ss,ex){
+  window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));};
+  (function(d,s){
+    fs=d.getElementsByTagName(s)[0];
+    function ce(src){
+      var cs=d.createElement(s);
+      cs.src=src;
+      cs.async=1;
+      fs.parentNode.insertBefore(cs,fs);
     };
-})();
+    ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js');
+  })(document,'script');
+})('JMvZ8gnoO1ma2pOd');
